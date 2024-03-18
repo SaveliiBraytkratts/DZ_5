eded85a3cab034784e9f4efb3da23a273b9719be
Савелий <braytkratts@bk.ru>
2024-03-19 09:08:26 +1000

    DZ5

--- src/test/java/org/max/home/accu/GetWeatherTenDayTest.java
+++ src/test/java/org/max/home/accu/GetWeatherTenDayTest.java
@@ -3,7 +3,6 @@ package org.max.home.accu;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +20,10 @@ import java.util.List;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GetWeatherOneDayTest extends AbstractTest{
+public class GetWeatherTenDayTest extends AbstractTest{
 
     private static final Logger logger
-            = LoggerFactory.getLogger(GetWeatherOneDayTest.class);
+            = LoggerFactory.getLogger(GetWeatherTenDayTest.class);
 
     @Test
     void get_shouldReturn200() throws IOException, URISyntaxException {
@@ -42,19 +40,19 @@ public class GetWeatherOneDayTest extends AbstractTest{
         dailyForecasts.add(dailyForecast);
         weather.setDailyForecasts(dailyForecasts);
 
-        logger.debug("Формирование мока для GET /forecasts/v1/daily/1day/294021");
-        stubFor(get(urlPathEqualTo("/forecasts/v1/daily/1day/294021"))
+        logger.debug("Формирование мока для GET /forecasts/v1/daily/10day/290396");
+        stubFor(get(urlPathEqualTo("/forecasts/v1/daily/10day/290396"))
                 .willReturn(aResponse()
                         .withStatus(200).withBody(mapper.writeValueAsString(weather))));
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        HttpGet request = new HttpGet(getBaseUrl() + "/forecasts/v1/daily/1day/294021");
+        HttpGet request = new HttpGet(getBaseUrl() + "/forecasts/v1/daily/10day/290396");
         logger.debug("http клиент создан");
         //when
         HttpResponse response = httpClient.execute(request);
         //then
-        verify(getRequestedFor(urlPathEqualTo("/forecasts/v1/daily/1day/294021")));
+        verify(getRequestedFor(urlPathEqualTo("/forecasts/v1/daily/10day/290396")));
         assertEquals(200, response.getStatusLine().getStatusCode());
 
         Weather responseBody = mapper.readValue(response.getEntity().getContent(), Weather.class);
@@ -67,17 +65,17 @@ public class GetWeatherOneDayTest extends AbstractTest{
     void get_shouldReturn500() throws IOException {
         logger.info("Тест код ответ 500 запущен");
         //given
-        logger.debug("Формирование мока для GET /forecasts/v1/daily/2day/294021");
-        stubFor(get(urlPathEqualTo("/forecasts/v1/daily/2day/294021"))
+        logger.debug("Формирование мока для GET /forecasts/v1/daily/15day/290396");
+        stubFor(get(urlPathEqualTo("/forecasts/v1/daily/15day/290396"))
                 .willReturn(aResponse()
                         .withStatus(500).withBody("ERROR")));
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet request = new HttpGet(getBaseUrl()+"/forecasts/v1/daily/2day/294021");
+        HttpGet request = new HttpGet(getBaseUrl()+"/forecasts/v1/daily/15day/290396");
         logger.debug("http клиент создан");
         //when
         HttpResponse response = httpClient.execute(request);
         //then
-        verify(getRequestedFor(urlPathEqualTo("/forecasts/v1/daily/2day/294021")));
+        verify(getRequestedFor(urlPathEqualTo("/forecasts/v1/daily/15day/290396")));
         assertEquals(500, response.getStatusLine().getStatusCode());
         assertEquals("ERROR", convertResponseToString(response));
     }
